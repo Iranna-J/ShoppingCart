f1984f682e5d7d4c1020b24253af4f2cce90b927
IrannaEcosmos <iranna@ecosmosolutions.com>
2025-02-28 13:36:56 +0530

    login

--- src/main/java/com/example/ShoppingCart/repo/LoginRepo.java
+++ src/main/java/com/example/ShoppingCart/repo/LoginRepo.java
@@ -1,5 +1,7 @@
 package com.example.ShoppingCart.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
@@ -8,7 +10,7 @@ import com.example.ShoppingCart.domain.User;
 
 @Repository
 @EnableJpaRepositories
-public interface UserRepo extends JpaRepository<User, Long> {
-	boolean existsByEmail(String email);
+public interface LoginRepo extends JpaRepository<User, Long>{
 	User findByEmail(String email);
+	Optional<User> findOneByEmailAndPassword(String email, String password);
 }
